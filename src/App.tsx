4f21a48d3f0b5ca42910853f6b648031ccc8b7a4
techsmit96 <sumitsakpal02@gmail.com>
2020-12-28 21:08:16 +0530

    added experience skills page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import MyNavbar from "./components/my-navbar/mynavbar.component";
 import TitleMessage from "./components/title-message/title-message.component";
 import About from "./pages/about/about.component";
 import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 import { Parallax } from "react-parallax";
 import { Container } from "react-bootstrap";
+import Skills from "./pages/skills/skills.component";
+import Experience from "./pages/experience/experience.component";
 
 const App = () => {
 	return (
@@ -16,7 +19,7 @@ const App = () => {
 			<TitleMessage />
 			<div>
 				<Parallax
-					blur={{ min: -30, max: 30 }}
+					blur={{ min: -1000, max: 1000 }}
 					bgImage={require("./assets/img/parallex/background.webp")}
 					bgImageAlt=''
 					strength={-200}
@@ -24,6 +27,31 @@ const App = () => {
 					<Container className='container-box rounded'>
 						<Fade duration={500}>
 							<About />
+						</Fade>
+					</Container>
+				</Parallax>
+			</div>
+			{/* Skills section  */}
+			<div>
+				<Container className='container-box rounded'>
+					<Slide bottom duration={500}>
+						<hr />
+						<Skills />
+					</Slide>
+				</Container>
+			</div>
+			{/* Experience  */}
+			<div>
+				<Parallax
+					blur={{ min: -1000, max: 1000 }}
+					bgImage={require("./assets/img/parallex/background.webp")}
+					bgImageAlt=''
+					strength={-200}
+				>
+					<Container className='container-box rounded'>
+						<Fade duration={500}>
+							<hr />
+							<Experience />
 						</Fade>
 					</Container>
 				</Parallax>
