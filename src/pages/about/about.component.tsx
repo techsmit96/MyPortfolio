239fa55c50b0f62a7978d0c49aa1b975f7a591b5
techsmit96 <sumitsakpal02@gmail.com>
2020-12-28 20:06:05 +0530

    added about page

--- src/pages/about/about.component.tsx
+++ src/pages/about/about.component.tsx
@@ -26,7 +26,8 @@ const About = () => {
 						<Col xs={12} md={6}>
 							{/* Description  */}
 							<Row className=' align-items-start p-2 my-details rounded'>
-								Hi there! I am <strong>&nbsp;Anand Kumar Jha</strong><br></br>
+								Hi there! I am <strong>&nbsp;Anand Kumar Jha</strong>
+								<br></br>
 								<br />A passionate programmer and a Coach, born and brought up
 								in India. I am a Full Stack Web Developer with React.js, Redux,
 								Express.js, Node.js, and PostgreSQL as my tech stack.
