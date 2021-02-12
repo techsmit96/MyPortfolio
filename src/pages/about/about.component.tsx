cce40e5d7bba29292382ce6db98195e610345045
Sumit <sumitsakpal02@gmail.com>
2021-02-12 23:15:40 +0530

    Final Upload

--- src/pages/about/about.component.tsx
+++ src/pages/about/about.component.tsx
@@ -57,7 +57,7 @@ const About = () => {
                     </div>
                     <div>
                       <a
-                        href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1QkdkFEFMDXZ3gLUzqx86VEg3eoH-orvl/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                       >
